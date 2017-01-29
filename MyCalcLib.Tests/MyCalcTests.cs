cf347ef9fcb601d5bdf9c5a2533e2accff4b14db
eugbor <eugeneborodin@mail.ru>
2017-01-29 04:07:23 +0300

    init

---
+++ MyCalcLib.Tests/MyCalcTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCalcLib.Tests
+{
+    [TestClass]
+    public class MyCalcTests
+    {
+        [TestMethod]
+        public void Sum_10and20_30returned()
+        {
+            // Arrange
+            int x = 10;
+            int y = 20;
+            int expected = 30;
+
+            //Act
+            MyCalc c = new MyCalc();
+            int actual = c.Sum(x, y);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ MyCalcLib/MyCalc.cs
@@ -0,0 +1,10 @@
+﻿namespace MyCalcLib
+{
+    public class MyCalc
+    {
+        public int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
